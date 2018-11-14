8f9237984ffb14e23d4d928e0fc743c30b07594a
Imran Qureshi <imran.qureshi@healthcatalyst.com>
2018-11-13 21:22:03 -0800

    switch from deployment to job
    Signed-off-by: Imran Qureshi <imran.qureshi@healthcatalyst.com>

--- install.ps1
+++ install.ps1
@@ -12,19 +12,27 @@ Start-Sleep -Seconds 10
 # https://docs.helm.sh/using_helm/
 Write-Host "Now installing for real"
 helm install ./certificategenerator `
-        --name certificategenerator `
-        --set-string certhostname=foo.foo.com `
-        --set-string certpassword=myfoo `
-        --set-string clientCertificateUser=fabricrabbitmquser `
-        --namespace kube-system `
-        --debug
+    --name certificategenerator `
+    --set-string certhostname=foo.foo.com `
+    --set-string certpassword=myfoo `
+    --set-string clientCertificateUser=fabricrabbitmquser `
+    --namespace kube-system `
+    --debug
+
+if (0 -ne $LastExitCode) {
+    Write-Error "helm threw an error"
+    exit 1
+}
 
 Write-Host "Listing packages"
 helm list
 
 kubectl get "deployments,pods,services,ingress,secrets" --namespace=$namespace -o wide
 
-Start-Sleep -Seconds 30
+Write-Host "--- Waiting for certificategenerator pod to complete ---"
+kubectl wait job --for=condition=complete --timeout=30s -l app=certificategenerator -n kube-system
+
+kubectl describe pod -l app=certificategenerator -n kube-system
 
 kubectl logs -l app=certificategenerator -n kube-system
 
